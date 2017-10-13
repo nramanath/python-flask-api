e8e60efae96376249b668e9864e9b45032bbd92c
Ramanathan Nachiappan <nramssn@gmail.com>
2017-10-13 00:06:57 -0700

    DELETE call

--- app.py
+++ app.py
@@ -72,6 +72,14 @@ def update_task(task_id):
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
